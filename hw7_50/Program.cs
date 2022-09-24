3c7cc8f07d157e6db82239f88a2b3a6c1781aac6
Dmitriy Filatov <filatov-d@mail.ru>
2022-09-24 13:16:21 +0300

    task 50 completed

--- hw7_50/Program.cs
+++ hw7_50/Program.cs
@@ -7,3 +7,56 @@
 8 4 2 4
 17 -> такого числа в массиве нет
 */
+
+int[,] FillArray(int m, int n)
+{
+    int[,] randomArray = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            randomArray[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return randomArray; 
+}
+
+void PrintResult(int rowArray, int columnArray, int m, int n, int[,] array)
+{
+    if (rowArray > m || columnArray > n)
+    {
+        Console.WriteLine("Такого числа в массиве нет");
+    }
+
+    else
+    {
+        Console.WriteLine($"Значение элемента на позиции {rowArray}, {columnArray}: {array[rowArray - 1, columnArray - 1]}");
+    }
+}
+
+Console.Write("Введите через пробел номера строки и столбца: ");
+string line = Console.ReadLine();
+
+string[] splitString = line.Split(" ");
+int rowArray = Convert.ToInt32(splitString[0]);
+int columnArray = Convert.ToInt32(splitString[1]);
+
+int m = 3, n = 4;
+int[,] array = FillArray(m, n);
+
+PrintResult(rowArray, columnArray, m, n, array);
+
+// Для проверки правильности выполнения задания ниже блок вывода массива:
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j]);
+            Console.Write(" ");
+
+        }
+        Console.WriteLine(" ");
+    }
+}
+
